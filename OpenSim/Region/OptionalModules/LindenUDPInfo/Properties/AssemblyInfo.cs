15ac5b891c4d19691ac4b35480bb15ee7602d47e
ft <ft@noemail>
2014-05-04 21:47:56 +0200

    project reorganized into smaller assemblies so users can select what they really need from the optional category

---
+++ OpenSim/Region/OptionalModules/LindenUDPInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using Mono.Addins;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenSim.Region.OptionalModules.LindenUDPInfo")]
+[assembly: AssemblyDescription("Linden UDP Info Commands Module for OpenSim")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpenSim.Region.OptionalModules.LindenUDPInfo.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6565855f-0f8e-4b13-a3da-ef76d8a0fc26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.8.0.*")]
+[assembly: Addin("OpenSim.Region.OptionalModules.LindenUDPInfo", "0.1")]
+[assembly: AddinDependency("OpenSim", "0.5")]
